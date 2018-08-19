3860aa6e1e83bbef89d326ae01b910159b5127cb
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-18 23:40:34 -0500

    implementacion de usuarios con preguntas

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,7 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :destroy, :edit]
   def index
     @questions = Question.all
+    @users = User.all
   end
  
   def new
@@ -10,7 +11,7 @@ class QuestionsController < ApplicationController
   
   def create
     @question = Question.new(question_params)
-    
+    @question.user = current_user
       if @question.save
           redirect_to questions_path
       else
@@ -24,20 +25,32 @@ class QuestionsController < ApplicationController
   
   def edit
     @question = Question.find(params[:id])
+    unless @question.user == current_user
+      redirect_to questions_path
+    end
   end
   
   def update
     @question = Question.find(params[:id])
-    if @question.update(question_params)
-      redirect_to questions_path, notice: "Success!"
-  else
-      render :edit
-  end
+    if @question.user == current_user
+      if @question.update(question_params)
+        redirect_to questions_path, notice: "Success!"
+      else
+        render :edit
+      end
+    end
   end
   
   def destroy
-    
+    @question = Question.find(params[:id])
+    if @question.user == current_user
+      question = Question.find(params[:id])
+      question.destroy
+      redirect_to questions_path
+    end    
   end
+
+private
 
   def question_params
     params.require(:question).permit(:title, :body)
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 class Question < ApplicationRecord
+    belongs_to :user
     validates :title, :body, presence: true
 end
