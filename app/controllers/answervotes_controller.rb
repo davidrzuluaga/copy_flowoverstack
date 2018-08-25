4aa773c40ad7fd692548e5144d45d4e405f6b17a
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-25 01:05:10 -0500

    fix votes counting

--- app/controllers/answervotes_controller.rb
+++ app/controllers/answervotes_controller.rb
@@ -1,15 +1,18 @@
 class AnswervotesController < ApplicationController
     def create
         answer = Answer.find(params[:answer_id])
-        answer.vote_answers.create(user: current_user)
-        
-        redirect_to question_path(Answer.find(params[:answer_id]).question_id)
+        if answer.vote_answers.where(user: current_user).count == 0
+            answer.vote_answers.create(user: current_user)
+            redirect_to question_path(Answer.find(params[:answer_id]).question_id)
+        end
     end
       
     def destroy
         answer = Answer.find(params[:answer_id])
-        answer.vote_answers.where(user: current_user).take.try(:destroy)
-        
-        redirect_to question_path(Answer.find(params[:answer_id]).question_id)
+        if answer.vote_answers.where(user: current_user).count == 1
+            answer.vote_answers.where(user: current_user).take.try(:destroy)
+            redirect_to question_path(Answer.find(params[:answer_id]).question_id)
+        end
     end
+
 end
--- app/controllers/questionvotes_controller.rb
+++ app/controllers/questionvotes_controller.rb
@@ -1,15 +1,18 @@
 class QuestionvotesController < ApplicationController
     def create
         question = Question.find(params[:question_id])
-        question.vote_questions.create(user: current_user)
-        
-        redirect_to question_path(params[:question_id])
+        if question.vote_questions.where(user: current_user).count == 0
+            question.vote_questions.create(user: current_user)
+            redirect_to question_path(params[:question_id])
+        end
     end
       
     def destroy
         question = Question.find(params[:question_id])
-        question.vote_questions.where(user: current_user).take.try(:destroy)
-        
-        redirect_to question_path(params[:question_id])
+        if question.vote_questions.where(user: current_user).count == 1
+            question.vote_questions.where(user: current_user).take.try(:destroy)
+            redirect_to question_path(params[:question_id])
+        end
+
     end
 end
